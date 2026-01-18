d559a761327c47bcf9ee7318e55ffd2f0405711c
Ayushi Pawar <aayushipawar2004@gmail.com>
2026-01-18 23:21:19 +0530

    Add files via upload

---
+++ 10. Print all Prime Number upto Nth term.py
@@ -0,0 +1,10 @@
+#Print all Prime Number upto Nth term 
+
+n = int(input())
+
+for num in range(2, n + 1):
+    for i in range(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num)
