3d3ccf56ef1b4aad21cbf81dcfac34250d47ade2
Ayushi Pawar <aayushipawar2004@gmail.com>
2026-01-22 22:32:00 +0530

    Add files via upload

---
+++ 20. Power of 2.py
@@ -0,0 +1,4 @@
+#  Powers of 2
+
+Power = int(input("Enter Power Number: ")) 
+print(2**Power)
---
+++ 21. Factorial.py
@@ -0,0 +1,5 @@
+# Factorial
+import math
+
+Number = int(input("Enter a Number: ")) 
+print(math.factorial(Number))
