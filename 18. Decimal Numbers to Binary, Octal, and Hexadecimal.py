f05a4d50002f1bace7ed225365ddf2d340d6d2b1
Ayushi Pawar <aayushipawar2004@gmail.com>
2026-01-22 20:41:48 +0530

    Add files via upload

---
+++ 18. Decimal Numbers to Binary, Octal, and Hexadecimal.py
@@ -0,0 +1,9 @@
+#  Decimal Numbers to Binary, Octal, and Hexadecimal 
+
+Number = int(input("Enter a Number: "))
+
+print("Decimal To Binary: ",(bin(Number)[2:]))
+
+print("Decimal To Octal: ",(oct(Number)[2:]))
+
+print("Decimal To Hexadecimal: ",(hex(Number)[2:]))
