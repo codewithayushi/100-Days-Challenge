47cd5bc19a27c52f328ba08b50107313bceedf5e
Ayushi Pawar <aayushipawar2004@gmail.com>
2026-01-26 23:15:15 +0530

    Add files via upload

---
+++ 32.  Fibonacci Sequence Using Recursive Functions.py
@@ -0,0 +1,10 @@
+# Fibonacci Sequence Using Recursive Functions i
+
+def fib(n):
+    a, b = 0, 1
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = int(input("Enter number of terms: "))
+fib(n)
